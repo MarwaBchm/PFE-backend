f5fd6c64e7a3e3edefe2e57a3aedbf9839acef3b
Bentoumi Merieme <bentoumimeriem21@gmail.com>
2025-01-11 20:20:10 +0100

    up1

--- pfe-backend/app/Http/Controllers/Controller.php
+++ pfe-backend/app/Http/Controllers/Controller.php
@@ -2,7 +2,27 @@
 
 namespace App\Http\Controllers;
 
-abstract class Controller
+use App\Mail\UserNotification;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Http\Request;
+
+class EmailController extends Controller
 {
-    //
+    public function sendEmail()
+    {
+        // Fetch user data from the database
+        $users = \DB::table('users')->get(); // Replace 'users' with your table name
+
+        foreach ($users as $user) {
+            $data = [
+                'name' => $user->name,
+                'message' => 'This is your notification message.',
+            ];
+
+            // Send email using the UserNotification Mailable
+            Mail::to($user->email)->send(new UserNotification($data));
+        }
+
+        return "Emails sent successfully!";
+    }
 }
--- pfe-backend/routes/web.php
+++ pfe-backend/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+Route::get('/send-email', [Controller::class, 'sendEmail']);
 
 Route::get('/', function () {
     return view('welcome');
