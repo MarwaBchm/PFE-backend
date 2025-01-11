fc1bcb7d52f6c33abaa5f698c79814e8fd5b9ff8
Bentoumi Merieme <bentoumimeriem21@gmail.com>
2025-01-11 22:32:43 +0100

    up11

--- pfe-backend/routes/web.php
+++ pfe-backend/routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-Route::get('/send-email', [Controller::class, 'sendEmail']);
+
+use App\Http\Controllers\EmailController;
+
+Route::get('/send-email', [EmailController::class, 'sendEmail']);
+// Route::get('/send-email', [Controller::class, 'sendEmail']);
 
 Route::get('/', function () {
     return view('welcome');
